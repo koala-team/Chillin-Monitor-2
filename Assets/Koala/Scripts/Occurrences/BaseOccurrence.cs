f2fff37dedb9eb479e130b42e71c0aa5a4971183
M6stafa <mostafa.najafi1996@gmail.com>
2018-10-31 21:42:51 +0330

    Fix a bug in change sibling order api

---
+++ Assets/Koala/Scripts/Occurrences/BaseOccurrence.cs
@@ -0,0 +1,63 @@
+﻿namespace Koala
+{
+	public interface IBaseOccurrence<T, C>
+	{
+		void Init(string reference,
+			float duration, C newConfig,
+			bool isForward, T forwardOccurrence);
+	}
+
+	public abstract class BaseOccurrence<T, C> : Occurrence, IBaseOccurrence<T, C>
+		where T : BaseOccurrence<T, C>
+		where C : class
+	{
+
+		protected string _reference;
+		protected float _duration;
+		protected C _oldConfig;
+		protected C _newConfig;
+		private bool _isForward;
+		protected T _forwardOccurrence;
+
+
+		public BaseOccurrence() { }
+
+		public void Init(string reference,
+			float duration, C newConfig,
+			bool isForward, T forwardOccurrence)
+		{
+			_reference = reference;
+			_duration = duration;
+			_newConfig = newConfig;
+			_isForward = isForward;
+			_forwardOccurrence = forwardOccurrence;
+		}
+
+		public override void Forward()
+		{
+			if (_isForward)
+			{
+				_oldConfig = CreateOldConfig();
+
+				ManageSuddenChanges(_newConfig, true);
+				ManageTweens(_newConfig, true);
+			}
+		}
+
+		public override void Backward()
+		{
+			if (!_isForward)
+			{
+				ManageTweens(_forwardOccurrence._oldConfig, false);
+			}
+			else
+			{
+				ManageSuddenChanges(_oldConfig, false);
+			}
+		}
+
+		protected abstract C CreateOldConfig();
+		protected virtual void ManageTweens(C config, bool isForward) { }
+		protected virtual void ManageSuddenChanges(C config, bool isForward) { }
+	}
+}
---
+++ Assets/Koala/Scripts/Occurrences/ChangeSiblingOrderOccurrence.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+namespace Koala
+{
+	public class ChangeSiblingOrderOccurrence : BaseOccurrence<ChangeSiblingOrderOccurrence, Director.ChangeSiblingOrderConfig>
+	{
+		private Transform _transform;
+
+
+		public ChangeSiblingOrderOccurrence() { }
+
+		protected override Director.ChangeSiblingOrderConfig CreateOldConfig()
+		{
+			var transform = GetTransform();
+
+			var oldConfig = new Director.ChangeSiblingOrderConfig
+			{
+				NewIndex = transform.GetSiblingIndex()
+			};
+
+			return oldConfig;
+		}
+
+		protected override void ManageSuddenChanges(Director.ChangeSiblingOrderConfig config, bool isForward)
+		{
+			var transform = GetTransform();
+
+			if (config.NewIndex.HasValue)
+				transform.SetSiblingIndex(config.NewIndex.Value);
+			else if (config.GotoFirst.HasValue && config.GotoFirst.Value)
+				transform.SetAsFirstSibling();
+			else if (config.GotoLast.HasValue && config.GotoLast.Value)
+				transform.SetAsLastSibling();
+			else if (config.ChangeIndex.HasValue)
+			{
+				int baseIndex;
+				if (config.SiblingReferenceAsBaseIndex != null)
+					baseIndex = References.Instance.GetGameObject(config.SiblingReferenceAsBaseIndex).transform.GetSiblingIndex();
+				else
+					baseIndex = transform.GetSiblingIndex();
+
+				transform.SetSiblingIndex(System.Math.Max(0, baseIndex + config.ChangeIndex.Value));
+			}
+		}
+
+		private Transform GetTransform()
+		{
+			if (_transform == null)
+				_transform = References.Instance.GetGameObject(_reference).transform;
+			return _transform;
+		}
+	}
+}
