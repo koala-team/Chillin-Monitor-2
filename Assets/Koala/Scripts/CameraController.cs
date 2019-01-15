09b8b962d88fcc6fa1129c4060b8077916599796
M6stafa <mostafa.najafi1996@gmail.com>
2019-01-15 17:29:01 +0330

    better camera controller performance

--- Assets/Koala/Scripts/CameraController.cs
+++ Assets/Koala/Scripts/CameraController.cs
@@ -10,14 +10,48 @@ namespace Koala
 
 		// wasd : basic movement
 		// shift : Makes camera accelerate
-		// space : Moves camera on X and Z axis only.  So camera doesn't gain any height*/
 
-		public Vector3 MinPosition { get; set; } = new Vector3(MIN_BOUNDRY, MIN_BOUNDRY, MIN_BOUNDRY);
-		public Vector3 MaxPosition { get; set; } = new Vector3(MAX_BOUNDRY, MAX_BOUNDRY, MAX_BOUNDRY);
-		public Vector2 MinRotation { get; set; } = new Vector2(MIN_BOUNDRY, MIN_BOUNDRY);
-		public Vector2 MaxRotation { get; set; } = new Vector2(MAX_BOUNDRY, MAX_BOUNDRY);
-		public float MinZoom { get; set; } = MIN_BOUNDRY;
-		public float MaxZoom { get; set; } = MAX_BOUNDRY;
+		private bool _positionBoundryChanged = false;
+		private Vector3 _minPosition = new Vector3(MIN_BOUNDRY, MIN_BOUNDRY, MIN_BOUNDRY);
+		private Vector3 _maxPosition = new Vector3(MAX_BOUNDRY, MAX_BOUNDRY, MAX_BOUNDRY);
+		public Vector3 MinPosition
+		{
+			get { return _minPosition; }
+			set { _positionBoundryChanged = true; _minPosition = value; }
+		}
+		public Vector3 MaxPosition
+		{
+			get { return _maxPosition; }
+			set { _positionBoundryChanged = true; _maxPosition = value; }
+		}
+
+		private bool _rotationBoundryChanged = false;
+		private Vector2 _minRotation = new Vector2(MIN_BOUNDRY, MIN_BOUNDRY);
+		private Vector2 _maxRotation = new Vector2(MAX_BOUNDRY, MAX_BOUNDRY);
+		public Vector2 MinRotation
+		{
+			get { return _minRotation; }
+			set { _rotationBoundryChanged = true; _minRotation = value; }
+		}
+		public Vector2 MaxRotation
+		{
+			get { return _maxRotation; }
+			set { _rotationBoundryChanged = true; _maxRotation = value; }
+		}
+
+		private bool _zoomBoundryChanged = false;
+		private float _minZoom = MIN_BOUNDRY;
+		private float _maxZoom = MAX_BOUNDRY;
+		public float MinZoom
+		{
+			get { return _minZoom; }
+			set { _zoomBoundryChanged = true; _minZoom = value; }
+		}
+		public float MaxZoom
+		{
+			get { return _maxZoom; }
+			set { _zoomBoundryChanged = true; _maxZoom = value; }
+		}
 
 		// camera movements
 		private const float MAIN_SPEED = 5.0f; // regular speed
@@ -39,23 +73,39 @@ namespace Koala
 			if (!Helper.GameStarted) return;
 
 			// Mouse
-			if (Input.GetMouseButton(0) && !EventSystem.current.IsPointerOverGameObject())
-			{
-				transform.Rotate(new Vector3(-Input.GetAxis("Mouse Y") * CAM_SENS, Input.GetAxis("Mouse X") * CAM_SENS, 0), Space.World);
-			}
-			transform.rotation = Quaternion.Euler(
-				Mathf.Clamp(transform.rotation.eulerAngles.x, MinRotation.x, MaxRotation.x),
-				Mathf.Clamp(transform.rotation.eulerAngles.y, MinRotation.y, MaxRotation.y),
-				0
-			);
+			float scrollDelta = 0;
+			bool rotated = false;
 
-			var scrollDelta = Input.mouseScrollDelta.y * SCROLL_SPEED;
-			if (scrollDelta != 0)
+			if (!EventSystem.current.IsPointerOverGameObject())
+			{
+				// Rotate
+				if (Input.GetMouseButton(0))
+				{
+					rotated = true;
+					transform.Rotate(new Vector3(-Input.GetAxis("Mouse Y") * CAM_SENS, Input.GetAxis("Mouse X") * CAM_SENS, 0), Space.World);
+				}
+
+				// Zoom
+				scrollDelta = Input.mouseScrollDelta.y * SCROLL_SPEED;
+			}
+
+			if (rotated || _rotationBoundryChanged)
+			{
+				transform.rotation = Quaternion.Euler(
+					Mathf.Clamp(transform.rotation.eulerAngles.x, MinRotation.x, MaxRotation.x),
+					Mathf.Clamp(transform.rotation.eulerAngles.y, MinRotation.y, MaxRotation.y),
+					0
+				);
+			}
+
+			if (scrollDelta != 0 || _zoomBoundryChanged)
 			{
 				if (_camera.orthographic)
 					_camera.orthographicSize = Mathf.Clamp(_camera.orthographicSize - scrollDelta, MinZoom, MaxZoom);
 				else
 					_camera.fieldOfView = Mathf.Clamp(_camera.fieldOfView - scrollDelta * 3, MinZoom, MaxZoom);
+
+				_zoomBoundryChanged = false;
 			}
 
 			//Keyboard commands
@@ -74,10 +124,13 @@ namespace Koala
 				p = p * MAIN_SPEED;
 			}
 
-			p = p * Time.deltaTime;
-			var newPosition = transform.position + p;
+			if (p != Vector3.zero || _positionBoundryChanged)
+			{
+				p = p * Time.deltaTime;
+				var newPosition = transform.position + p;
 
-			transform.position = newPosition.Clamp(MinPosition, MaxPosition);
+				transform.position = newPosition.Clamp(MinPosition, MaxPosition);
+			}
 
 			//if (Input.GetKey(KeyCode.Space)) // If player wants to move on X and Z axis only
 			//{
@@ -95,7 +148,7 @@ namespace Koala
 
 		private Vector3 GetBaseInput() // returns the basic values, if it's 0 than it's not active.
 		{
-			Vector3 p_Velocity = new Vector3();
+			Vector3 p_Velocity = Vector3.zero;
 
 			if (Input.GetKey(KeyCode.W))
 			{
